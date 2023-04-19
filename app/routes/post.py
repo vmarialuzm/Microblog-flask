70603e4d4aba927588fe9f172834287460c6ad14
vmarialuzm <vmarialuzm@gmail.com>
2023-04-19 18:57:14 -0500

    add api post

--- app/routes/post.py
+++ app/routes/post.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template,redirect,url_for
+from flask import Blueprint,render_template,redirect,url_for,request,jsonify
 from flask_login import current_user,login_required
 from app.db import db
 from app.models.posts import Post
@@ -33,3 +33,16 @@ def post_detail(id):
     }
 
     return render_template("post-detail.html", **context)
+
+@post_router.route("/api/post",methods=["POST"])
+def api_post():
+    #esta es la informacion que nos envia el cliente
+    #recuerden que con .json transforma la data y puedo leerlo desde py
+    #para este caso solo requiero 2 cosas body, user_id
+    #{"body": "Texto del post","user_id": 1}
+    client_post_information = request.json
+    new_post=Post(body=client_post_information["body"],user_id=client_post_information["user_id"])
+    db.session.add(new_post)
+    db.session.commit()
+    #nota: el estado de una creacion es 201:created
+    return (jsonify(new_post.to_json(),201))
