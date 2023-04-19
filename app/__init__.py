e03fce1cd984be6dced0d1bfcc0cf4a8a8ba038f
vmarialuzm <vmarialuzm@gmail.com>
2023-04-19 17:59:26 -0500

    add blue print

--- app/__init__.py
+++ app/__init__.py
@@ -5,17 +5,20 @@ from .config import Config
 from app.db import db
 from flask_migrate import Migrate
 
-from app.models.posts import Post
-from app.models.usuarios import User
-from app.models.roles import Role
-
+from app.routes.post import post_router
+from app.routes.auth import auth_router
+from app.routes.index import index_router
 
 def create_app():
     app = Flask(__name__)
     Bootstrap(app)
     app.config.from_object(Config)
 
+    app.register_blueprint(post_router)
+    app.register_blueprint(auth_router)
+    app.register_blueprint(index_router)
+
     #db = SQLAlchemy(app)
-    migrate = Migrate(app, db)
+    Migrate(app, db)
 
     return app
